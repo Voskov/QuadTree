61d89c6a7349387d68349c19168ede415f57fa38
Ariel Voskov <Ariel.Voskov@OzVision.com>
2014-06-16 09:08:41 +0300

    Added stuff and tested

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,48 +1,65 @@
 import java.util.HashSet;
 
 public class QuadTree {
-    private Node root;
+    private Node node;
 
-    public Node getRoot() {
-        return root;
+    public Node getNode() {
+        return node;
     }
 
     public void insert(double x, double y) {
-        if (root == null) {
-            root = new Node(x, y);
+        if (node == null) {
+            node = new Node(x, y);
         } else {
-            insert(root, x, y);
+            insert(node, x, y);
         }
     }
 
     public void insert(Node node, double x, double y) {
-        if (x <= node.x && y <= node.y) node.SW.insert(x, y);
-        else if (x <= node.x && y > node.y) node.NW.insert(x, y);
-        else if (x > node.x && y <= node.y) node.SE.insert(x, y);
+        if (x <= node.x && y <= node.y)  {
+            if (node.SW == null) {
+                node.SW = new QuadTree();
+            }
+            node.SW.insert(x, y);
+        }
+        else if (x <= node.x && y > node.y) {
+            if (node.NW == null) {
+                node.NW = new QuadTree();
+            }
+            node.NW.insert(x, y);
+        }
+        else if (x > node.x && y <= node.y)  {
+            if (node.SE == null) {
+                node.SE = new QuadTree();
+            }
+            node.SE.insert(x, y);
+        }
         else if (x > node.x && y > node.y) {
-            node.NE = new QuadTree();
+            if (node.NE == null) {
+                node.NE = new QuadTree();
+            }
             node.NE.insert(x, y);
         }
     }
 
     public boolean isWithin(Node center, Node other, double radius) {
-        double distance = Math.sqrt(other.x * other.x + other.y * other.y);
+        double distance = Math.sqrt((center.x - other.x) * (center.x - other.x) + (center.y - other.y) * (center.y - other.y));
         return (distance < radius);
     }
 
     public HashSet allWithin(QuadTree tree, Node center, double radius) {
+        if (tree == null) return null;
         HashSet allNodes = new HashSet<Node>();
-        if (isWithin(center, this.root, radius)) {
-            allNodes.add(this.root);
-            allNodes.add(this.root.NE);
-            allNodes.add(this.root.NW);
-            allNodes.add(this.root.SE);
-            allNodes.add(this.root.SW);
-        }
-        else if (center.x >= root.x && center.y >= root.y) allNodes.add(allWithin(this.root.NE, center, radius));
-        else if (center.x >= root.x && center.y < root.y) allNodes.add(allWithin(this.root.SE, center, radius));
-        else if (center.x < root.x && center.y >= root.y) allNodes.add(allWithin(this.root.NW, center, radius));
-        else if (center.x < root.x && center.y < root.y) allNodes.add(allWithin(this.root.SW, center, radius));
+        if (isWithin(center, tree.node, radius)) {
+            allNodes.add(tree.node);
+            allNodes.add(allWithin(tree.node.NE, center, radius));
+            allNodes.add(allWithin(tree.node.NW, center, radius));
+            allNodes.add(allWithin(tree.node.SE, center, radius));
+            allNodes.add(allWithin(tree.node.SW, center, radius));
+        } else if (center.x >= node.x && center.y >= node.y) allNodes.add(allWithin(tree.node.NE, center, radius));
+        else if (center.x >= node.x && center.y < node.y) allNodes.add(allWithin(tree.node.SE, center, radius));
+        else if (center.x < node.x && center.y >= node.y) allNodes.add(allWithin(tree.node.NW, center, radius));
+        else if (center.x < node.x && center.y < node.y) allNodes.add(allWithin(tree.node.SW, center, radius));
         return allNodes;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,13 @@
+import java.util.HashSet;
+
 public class Test {
     public static void main(String[] args) {
         QuadTree t = new QuadTree();
-        t.insert(2, 4);
-        t.insert(3, 5);
-        System.out.println(" ");
+        t.insert(0,0);
+        t.insert(0,1);
+        t.insert(-1,0);
+        t.insert(1,1);
+        HashSet all = t.allWithin(t, new Node(2, 2), 2.0);
+        System.out.println(all.toString());
     }
 }
