b5bc253606cd3907e7d35de9fd5308d1e86fb009
Ariel Voskov <Ariel.Voskov@OzVision.com>
2014-06-16 08:46:54 +0300

    Add allWithin

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,5 +1,11 @@
+import java.util.HashSet;
+
 public class QuadTree {
     private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
 
     public void insert(double x, double y) {
         if (root == null) {
@@ -16,8 +22,27 @@ public class QuadTree {
         else if (x > node.x && y > node.y) {
             node.NE = new QuadTree();
             node.NE.insert(x, y);
-        } ;
+        }
     }
 
+    public boolean isWithin(Node center, Node other, double radius) {
+        double distance = Math.sqrt(other.x * other.x + other.y * other.y);
+        return (distance < radius);
+    }
 
+    public HashSet allWithin(QuadTree tree, Node center, double radius) {
+        HashSet allNodes = new HashSet<Node>();
+        if (isWithin(center, this.root, radius)) {
+            allNodes.add(this.root);
+            allNodes.add(this.root.NE);
+            allNodes.add(this.root.NW);
+            allNodes.add(this.root.SE);
+            allNodes.add(this.root.SW);
+        }
+        else if (center.x >= root.x && center.y >= root.y) allNodes.add(allWithin(this.root.NE, center, radius));
+        else if (center.x >= root.x && center.y < root.y) allNodes.add(allWithin(this.root.SE, center, radius));
+        else if (center.x < root.x && center.y >= root.y) allNodes.add(allWithin(this.root.NW, center, radius));
+        else if (center.x < root.x && center.y < root.y) allNodes.add(allWithin(this.root.SW, center, radius));
+        return allNodes;
+    }
 }
